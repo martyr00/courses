c7389d73a8cef95d211eb4906e5ece667106fbc4
martyr00 <leonyvb04@gmail.com>
2023-06-13 01:00:31 +0300

    Creating models for an api application

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Courses
 
-# Create your models here.
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        name = 'categories'
+        allowed_methods = ['get']
+
+
+class CoursesResource(ModelResource):
+    class Meta:
+        queryset = Courses.objects.all()
+        name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
--- base/settings.py
+++ base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
