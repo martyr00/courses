78e30df8066309d152b3f5c8d370289c000af2e0
martyr00 <leonyvb04@gmail.com>
2023-06-13 01:36:01 +0300

    Configuring Authentication and Authorization for API

---
+++ api/authentication.py
@@ -0,0 +1,9 @@
+from tastypie.authentication import ApiKeyAuthentication
+
+
+class CustomAuthentication(ApiKeyAuthentication):
+    def is_authenticated(self, request, **kwargs):
+        if request.method is 'GET':
+            return True
+
+        return super().is_authenticated(request, **kwargs)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from tastypie.resources import ModelResource
 from shop.models import Category, Courses
+from .authentication import CustomAuthentication
+from tastypie.authorization import Authorization
 
 
 class CategoryResource(ModelResource):
@@ -14,3 +16,5 @@ class CourseResource(ModelResource):
         queryset = Courses.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
+        authorization = Authorization()
+        authentication = CustomAuthentication()
