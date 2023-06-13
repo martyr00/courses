f2830f186f47b7cc9e3471ade8ebde1098b74599
martyr00 <leonyvb04@gmail.com>
2023-06-13 15:06:20 +0300

    refactorer admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
-from . import models
+from .models import Category, Courses
 
-admin.site.register(models.Category)
-admin.site.register(models.Courses)
+admin.site.site_title = 'Courses admin panel'
+admin.site.site_header = 'Courses'
+admin.site.index_title = 'Welcome to admin area'
+
+
+class CoursesInline(admin.TabularInline):
+    model = Courses
+    exclude = ['created_at']
+    extra = 1
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Dates', {
+            'fields': ['created_at'],
+            'classes': ['collapse']
+        })
+    ]
+    inlines = [CoursesInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Courses, CourseAdmin)
